d60379aac2e9fbe2db9768a7d99557b65871ea7a
Raizaz Achmad Asyraf <raizaz.achmad.asyraf@gmail.com>
2022-09-22 06:53:41 +0700

    test.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        responseHTML = self.client.get('/mywatchlist/html/')
+        responseXML = self.client.get('/mywatchlist/xml/')
+        responseJSON = self.client.get('/mywatchlist/json/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(responseHTML.status_code, 200)
+        self.assertEqual(responseXML.status_code, 200)
+        self.assertEqual(responseJSON.status_code, 200)
